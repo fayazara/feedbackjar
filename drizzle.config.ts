330fc19927578be78042dc8f0f1158e6053b93ba
fayazara <fayazara@gmail.com>
2023-07-28 17:41:05 +0530

    fix: Handle drizzle studio in local env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,29 @@
 import "dotenv/config";
 import { Config } from "drizzle-kit";
+const { NODE_ENV, TURSO_DB_URL, TURSO_DB_TOKEN } = process.env;
+const isDev = NODE_ENV === "development";
 
-export default <Config>{
-  out: "./server/db/migrations",
-  schema: "./server/db/schema.ts",
-  breakpoints: true,
-  driver: "turso",
+// Local
+const sqlite = {
+  driver: "better-sqlite",
   dbCredentials: {
-    url: process.env.TURSO_DB_URL,
-    authToken: process.env.TURSO_DB_TOKEN,
+    url: "./server/db/db.sqlite",
   },
 };
+
+// Turso
+const turso = {
+  driver: "turso",
+  dbCredentials: {
+    url: TURSO_DB_URL,
+    authToken: TURSO_DB_TOKEN,
+  },
+};
+
+const dbCreds = isDev ? sqlite : turso;
+
+export default {
+  schema: "./server/db/schema.ts",
+  out: "./server/db/migrations",
+  ...dbCreds,
+} satisfies Config;
